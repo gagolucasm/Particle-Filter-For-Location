38dae8cf0d9a838e740ba363333e570c65bf66a2
Lucas Gago <gagolucasm@hotmail.com>
2016-10-11 15:31:45 +0200

    Update GetLandMark_error.m

--- GetLandMark_error.m
+++ GetLandMark_error.m
@@ -1,5 +1,6 @@
 function [ sal ] = GetLandMark_error( rpos,map,land )
-
+% Function used for getting the distance and angle between the robot and the landmark
+% Adding some error
 rpos(3)=AngleWrap(rpos(3));
 ang=atan2(+rpos(1)-map(1,land),rpos(2)-map(2,land))-rpos(3);
 dist=sqrt((rpos(1)-map(1,land))^2+(rpos(2)-map(2,land))^2);
